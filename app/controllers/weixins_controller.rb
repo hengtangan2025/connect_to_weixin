9cbffb03a737314a7310ba6dbeb5e4fc49271bf9
hengtangan2025 <504371515@qq.com>
2016-05-05 11:38:04 +0800

    测试连接微信

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -1,7 +1,9 @@
 class WeixinsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_filter :check_weixin_legality
+  # before_filter :check_weixin_legality
   def show
+     array = [Rails.configuration.weixin_token,params[:timestamp],params[:nonce]].sort
+      render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
     render :text => params[:echostr]
   end
 
@@ -11,9 +13,9 @@ class WeixinsController < ApplicationController
     end
   end
 
-  private
-  def check_weixin_lagality
-    array = [Rails.configuration.weixin_token,params[:timestamp],params[:nonce]].sort
-    render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
-  end
+  # private
+  #   def check_weixin_lagality
+  #     array = [Rails.configuration.weixin_token,params[:timestamp],params[:nonce]].sort
+  #     render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+  #   end
 end
