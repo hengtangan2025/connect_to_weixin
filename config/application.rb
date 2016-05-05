b253c6562132714ea568b7fa7d30557d8188215a
hengtangan2025 <504371515@qq.com>
2016-05-05 11:13:48 +0800

    测试连接微信

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ConnectToWeixin
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # 微信 API token
+    config.weixin_token = "kc_courses"
   end
 end
