356da6e43285b4885d2fb44ccf66d1dea6c3d4f3
hengtangan2025 <504371515@qq.com>
2016-05-27 11:54:10 +0800

    0527 1154

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -2,27 +2,13 @@ class WeixinsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   before_filter :check_weixin_legality
   def show
-    # token = "kc_courses"
-    # array = [token,params[:timestamp],params[:nonce]].sort
-    # if params[:signature] != Digest::SHA1.hexdigest(array.join)
-    #   render :text => "Forbidden", :status => 403
-    # else
     render :text => params[:echostr]
-    # end
   end
 
   def create
-    # token = "kc_courses"
-    # array = [token,params[:timestamp],params[:nonce]].sort
-    # if params[:signature] != Digest::SHA1.hexdigest(array.join)
-    #   render :text => "Forbidden", :status => 403
-    # else 
     if params[:xml][:MsgType] == "text"
-      p params[:xml][:Content]
       render "echo", :formats => :xml
-      # render :text => params[:echostr]
     end
-    # end
   end
 
   def echo
