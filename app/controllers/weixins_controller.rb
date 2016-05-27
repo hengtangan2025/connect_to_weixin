9f56229aab21e1784b61a06597743fc09110a06f
hengtangan2025 <504371515@qq.com>
2016-05-27 11:49:25 +0800

    0527 1149

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -1,36 +1,38 @@
 class WeixinsController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_filter :check_weixin_legality
   def show
-    token = "kc_courses"
-    array = [token,params[:timestamp],params[:nonce]].sort
-    if params[:signature] != Digest::SHA1.hexdigest(array.join)
-      render :text => "Forbidden", :status => 403
-    else
-      render :text => params[:echostr]
-    end
+    # token = "kc_courses"
+    # array = [token,params[:timestamp],params[:nonce]].sort
+    # if params[:signature] != Digest::SHA1.hexdigest(array.join)
+    #   render :text => "Forbidden", :status => 403
+    # else
+    render :text => params[:echostr]
+    # end
   end
 
   def create
-    token = "kc_courses"
-    array = [token,params[:timestamp],params[:nonce]].sort
-    if params[:signature] != Digest::SHA1.hexdigest(array.join)
-      render :text => "Forbidden", :status => 403
-    else 
-      if params[:xml][:MsgType] == "text"
-        p params[:xml][:Content]
-        render "echo", :formats => :xml
-        render :text => params[:echostr]
-     end
+    # token = "kc_courses"
+    # array = [token,params[:timestamp],params[:nonce]].sort
+    # if params[:signature] != Digest::SHA1.hexdigest(array.join)
+    #   render :text => "Forbidden", :status => 403
+    # else 
+    if params[:xml][:MsgType] == "text"
+      p params[:xml][:Content]
+      render "echo", :formats => :xml
+      # render :text => params[:echostr]
     end
+    # end
   end
 
   def echo
     
   end
 
-  # private
-  #   def check_weixin_lagality
-  #     array = ["kc_courses",params[:timestamp],params[:nonce]].sort
-  #     render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
-  #   end
+  private
+    def check_weixin_lagality
+      token = "kc_courses"
+      array = [token,params[:timestamp],params[:nonce]].sort
+      render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+    end
 end
