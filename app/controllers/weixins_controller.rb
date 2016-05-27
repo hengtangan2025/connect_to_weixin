8e7598c02f54753c81a009cccad35a40aef1318b
hengtangan2025 <504371515@qq.com>
2016-05-27 11:21:45 +0800

    0527 1121

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -1,46 +1,28 @@
 class WeixinsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-  # before_filter :check_weixin_legality
+  before_filter :check_weixin_legality
   def show
-    token = "kc_courses"
-    array = [token,params[:timestamp],params[:nonce]].sort
-    if params[:signature] != Digest::SHA1.hexdigest(array.join)
-      render :text => "Forbidden", :status => 403
-    else
-      render :text => params[:echostr]
-    end
+    # token = "kc_courses"
+    # array = [token,params[:timestamp],params[:nonce]].sort
+    # if params[:signature] != Digest::SHA1.hexdigest(array.join)
+    #   render :text => "Forbidden", :status => 403
+    # else
+    render :text => params[:echostr]
+    # end
   end
 
   def create
-    token = "kc_courses"
-    array = [token,params[:timestamp],params[:nonce]].sort
-    if params[:signature] != Digest::SHA1.hexdigest(array.join)
-      render :text => "Forbidden", :status => 403
-    else
-      render :text => params[:echostr]
-    end
+    # token = "kc_courses"
+    # array = [token,params[:timestamp],params[:nonce]].sort
+    # if params[:signature] != Digest::SHA1.hexdigest(array.join)
+    #   render :text => "Forbidden", :status => 403
+    # else
+    #   render :text => params[:echostr]
+    # end
 
 
     if params[:xml][:MsgType] == "text"
-      p params[:xml][:Content]
-      hash = {}
-      hash[:ToUserName] = params[:xml][:FromUserName]
-      hash[:FromUserName] = params[:xml][:ToUserName] 
-      hash[:CreateTime] = Time.now.to_i 
-      hash[:MsgType] = params[:xml][:MsgType] 
-      hash[:Content] = params[:xml][:Content]
-      render :xml =>{:result => hash.to_xml}
-      # xml_data = "<xml>
-      # <ToUserName><![CDATA[<%= params[:xml][:FromUserName] %>]]></ToUserName>
-      # <FromUserName><![CDATA[<%= params[:xml][:ToUserName] %>]]></FromUserName>
-      # <CreateTime><%= Time.now.to_i %></CreateTime>
-      # <MsgType><![CDATA[text]]></MsgType>
-      # <Content><![CDATA[大山的回声：<%= params[:xml][:Content] %>]]></Content>
-      # <FuncFlag>0</FuncFlag>
-      # </xml>"
-      # respond_to do |format|
-      #   format.xml {render xml: xml_data}
-      # end
+      p params[:xml][:content]
+      render "echo", :formats => :xml
     end
   end
 
@@ -48,9 +30,9 @@ class WeixinsController < ApplicationController
     
   end
 
-  # private
-  #   def check_weixin_lagality
-  #     array = [Rails.configuration.weixin_token,params[:timestamp],params[:nonce]].sort
-  #     render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
-  #   end
+  private
+    def check_weixin_lagality
+      array = ["kc_courses",params[:timestamp],params[:nonce]].sort
+      render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+    end
 end
